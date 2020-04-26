fb37a0df0fa43c15058f7ac70f80b6386acf3678
veit <veit.frick@aau.at>
2020-04-26 13:41:36 +0200

    Minor UI stuff

--- Assets/CustomerDisplay.cs
+++ Assets/CustomerDisplay.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CustomerDisplay : MonoBehaviour
@@ -7,11 +8,27 @@ public class CustomerDisplay : MonoBehaviour
 
     public GameObject shoppingListTemplate;
     public CustomerList cList;
-   
+
+
     void Start()
     {
-        int counter = 0;
-        cList.shoppingListList.ForEach(delegate (ShoppingList sList)
+        //int counter = 0;
+        List<ShoppingList> sortedList = cList.shoppingListList.OrderBy(o => o.spawnDelay).ToList();
+
+
+        //If the elements in the list are not destroyed after the customer has finished - check for hide or other property that shows that the customer is done and ignore said customer
+        for (int i = 0; i < sortedList.Count && i < 4; i++) // Loop through List with for
+        {
+            GameObject slobject = Instantiate(shoppingListTemplate) as GameObject;
+            slobject.GetComponent<ListDisplay>().SetValues(sortedList[i]);
+            slobject.SetActive(true);
+            slobject.transform.SetParent(shoppingListTemplate.transform.parent);
+            slobject.transform.localPosition = new Vector2(0, 340 + i * -210);
+        }
+
+
+        /*
+        sortedList.ForEach(delegate (ShoppingList sList)
         {
             Debug.Log("Here: " + sList);
             GameObject slobject = Instantiate(shoppingListTemplate) as GameObject;
@@ -20,7 +37,10 @@ public class CustomerDisplay : MonoBehaviour
             slobject.transform.SetParent(shoppingListTemplate.transform.parent);
             slobject.transform.localPosition = new Vector2(0, 340 + counter * -210);
             counter++;
+            if(counter>3)
+                break;
 
         });
+        */
     }
 }
